78499dfb95ef88e80916389595d6fceb22e32861
asdasd571 <kng955626@gmail.com>
2025-03-06 13:42:18 +0900

    15663 N과M(9)

--- 15663_NM_9.py
+++ 15663_NM_9.py
@@ -2,22 +2,23 @@ import sys
 input = sys.stdin.readline
 
 N,M = map(int,input().split())
-
-arr = list(map(int,input().split()))
-arr_set = set(arr)
-arr_set = sorted(arr_set)
+arr = sorted(list(map(int,input().split())))
+visited = [0] * N
 s = []
 
-def dfs(start):
-    if len(s)==M:
-        print(' '.join(map(str,s)))
+def dfs():
+    check_num = 0
+
+    if len(s) == M:
+        print(" ".join(map(str,s)))
         return
-
-    for i in arr_set:
-        
-        s.append(i)
-        dfs(i)
-        s.pop()
-
-dfs(min(arr_set))
-
+    
+    for i in range(N):            
+        if check_num != arr[i] and visited[i]==0:
+            s.append(arr[i])
+            visited[i] = 1
+            check_num = arr[i]
+            dfs()
+            visited[i] = 0
+            s.pop()
+dfs()
